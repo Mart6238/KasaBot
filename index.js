7865cfcfacc80b8208287b32f3fbe55480e1b7c2
Mart6238 <45314028+Mart6238@users.noreply.github.com>
2018-11-24 17:43:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const cfg = require('./index.json');
+const prefix = (cfg.prefix);
+bot.on('ready', function () (
+    console.log("c'est parti !!!")
+    bot.user.setActivity('Regardez moi !!!').catch(console.error)
+});
 bot login (NTE1ODIzNzY1NTYxODAyNzUy.Dtr9gw.NHF4JBfqbONsN0DlMHzCx7CNalY)
-
-client.on('message', message => {
-    if (message.content === 'k!help') {
-      message.reply('Les commandes sont :
-      k!ping : répond pong ');
