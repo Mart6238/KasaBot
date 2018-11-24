9b528b4011c53bb5b3ce2a4015527291591cd8c1
Mart6238 <45314028+Mart6238@users.noreply.github.com>
2018-11-24 17:28:09 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+bot login (NTE1ODIzNzY1NTYxODAyNzUy.Dtr9gw.NHF4JBfqbONsN0DlMHzCx7CNalY)
+
+client.on('message', message => {
+    if (message.content === 'k!help') {
+      message.reply('Les commandes sont :
+      k!ping : répond pong ');
