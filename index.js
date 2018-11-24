17881538b07acec31f154e40bea0f43906f643c2
Mart6238 <45314028+Mart6238@users.noreply.github.com>
2018-11-24 17:54:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const cfg = require('./index.json');
 const prefix = (cfg.prefix);
+const token = process.env.token;
 bot.on('ready', function () (
     console.log("c'est parti !!!")
     bot.user.setActivity('Regardez moi !!!').catch(console.error)
@@ -9,4 +10,4 @@ bot.on('ready', function () (
 
 
 
-bot login (NTE1ODIzNzY1NTYxODAyNzUy.Dtr9gw.NHF4JBfqbONsN0DlMHzCx7CNalY)
+bot login (token)
